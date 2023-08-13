8551a04ff4eb4d70b804dea9e82d2be74a2a82eb
1582421598 <26507452+1582421598@users.noreply.github.com>
2023-08-14 05:21:13 +0800

    some sync updates and fix kuwo api

--- src/provider/kuwo.js
+++ src/provider/kuwo.js
@@ -1,28 +1,28 @@
-const insure = require('./insure');
-const select = require('./select');
-const crypto = require('../crypto');
-const request = require('../request');
-const cache = require('../cache');
+const insure = require("./insure");
+const select = require("./select");
+const crypto = require("../crypto");
+const request = require("../request");
+const cache = require("../cache");
+const { encrypt } = require("../kwDES");
 
 const format = (song) => ({
-	id: song.musicrid.split('_').pop(),
+	id: song.musicrid.split("_").pop(),
 	name: song.name,
 	// duration: song.songTimeMinutes.split(':').reduce((minute, second) => minute * 60 + parseFloat(second), 0) * 1000,
 	duration: song.duration * 1000,
 	album: {
 		id: song.albumid,
-		name: song.album
+		name: song.album,
 	},
-	artists: song.artist
-		.split('&')
-		.map((name, index) => ({
-			id: index ? null : song.artistid,
-			name
-		})),
-	weight: 0
+	artists: song.artist.split("&").map((name, index) => ({
+		id: index ? null : song.artistid,
+		name,
+	})),
+	weight: 0,
 });
 
-var weight = 0
+const encryptKey = "Hm_Iuvt_cdb524f42f0cer9b268e4v7y734w5esq24";
+var weight = 0;
 
 const search = (info) => {
 	// const url =
@@ -52,73 +52,69 @@ const search = (info) => {
 	// 		return Promise.reject()
 	// })
 
-	const keyword = encodeURIComponent(info.keyword.replace(' - ', ' '));
+	const keyword = encodeURIComponent(info.keyword.replace(" - ", " "));
 	const url = `http://www.kuwo.cn/api/www/search/searchMusicBykeyWord?key=${keyword}&pn=1&rn=5`;
 
-	return request('GET', `http://kuwo.cn/search/list?key=${keyword}`)
+	return request("GET", `http://kuwo.cn/search/list?key=${keyword}`)
 		.then((response) =>
-			response.headers['set-cookie']
-			.find((line) => line.includes('kw_token'))
-			.replace(/;.*/, '')
-			.split('=')
-			.pop()
+			response.headers["set-cookie"]
+				.find((line) => line.includes(encryptKey))
+				.replace(/;.*/, "")
+				.split("=")
+				.pop()
 		)
-		.then((token) =>
-			request('GET', url, {
-				referer: `http://www.kuwo.cn/search/list?key=${keyword}`,
-				csrf: token,
-				cookie: `kw_token=${token}`,
+		.then((data) =>
+			request("GET", url, {
+				Referer: `http://www.kuwo.cn/search/list?key=${keyword}`,
+				Cookie: `${encryptKey}=${data}`,
+				Secret: crypto.kuwoapi.SecretCalc(data, encryptKey),
 			})
 		)
 		.then((response) => response.json())
 		.then((jsonBody) => {
-			if (
-				jsonBody &&
-				typeof jsonBody === 'object' &&
-				'code' in jsonBody &&
-				jsonBody.code !== 200
-			)
+			if (!jsonBody || jsonBody.code !== 200 || jsonBody.data.total < 1)
 				return Promise.reject();
 			const list = jsonBody.data.list.map(format);
-			const matched = select.selectList(list, info)
-			weight = matched.weight
+			const matched = select.selectList(list, info);
+			weight = matched.weight;
 			return matched ? matched.id : Promise.reject();
 		});
 };
 
 const track = (id) => {
-	const url = crypto.kuwoapi ?
-		'http://mobi.kuwo.cn/mobi.s?f=kuwo&q=' +
-		crypto.kuwoapi.encryptQuery(
-			'corp=kuwo&p2p=1&type=convert_url2&sig=0&format=' + ['flac', 'mp3']
-			.slice(select.ENABLE_FLAC ? 0 : 1)
-			.join('|') +
-			'&rid=' +
-			id
-		) :
-		'http://antiserver.kuwo.cn/anti.s?type=convert_url&format=mp3&response=url&rid=MUSIC_' +
-		id; // flac refuse
+	const url = crypto.kuwoapi
+		? "http://mobi.kuwo.cn/mobi.s?f=kuwo&q=" +
+		  crypto.kuwoapi.encryptQuery(
+				"corp=kuwo&p2p=1&type=convert_url2&sig=0&format=" +
+					["flac", "mp3"]
+						.slice(select.ENABLE_FLAC ? 0 : 1)
+						.join("|") +
+					"&rid=" +
+					id
+		  )
+		: "http://antiserver.kuwo.cn/anti.s?type=convert_url&format=mp3&response=url&rid=MUSIC_" +
+		  id; // flac refuse
 	// : 'http://www.kuwo.cn/url?format=mp3&response=url&type=convert_url3&br=320kmp3&rid=' + id // flac refuse
 
-	return request('GET', url, {
-			'user-agent': 'okhttp/3.10.0'
-		})
+	return request("GET", url, {
+		"user-agent": "okhttp/3.10.0",
+	})
 		.then((response) => response.body())
 		.then((body) => {
 			const url = (body.match(/http[^\s$"]+/) || [])[0];
 			if (url) {
 				return {
 					url: url,
-					weight: weight
-				}
+					weight: weight,
+				};
 			} else Promise.reject();
 		})
 		.catch(() => insure().kuwo.track(id));
 };
 
-const check = info => cache(search, info).then(track)
+const check = (info) => cache(search, info).then(track);
 
 module.exports = {
 	check,
-	track
+	track,
 };
--- src/provider/pyncmd.js
+++ src/provider/pyncmd.js
@@ -3,44 +3,44 @@ const request = require("../request");
 const cache = require("../cache");
 
 const track = (info) => {
-    const url =
-        ["https://pyncmd.vercel.app", "http://76.76.21.114"].slice(
-            select.CAN_ACCESS_GOOGLE ? 0 : 1,
-            select.CAN_ACCESS_GOOGLE ? 1 : 2
-        ) +
-        "/api/pyncm?module=track&method=GetTrackAudio&song_ids=" +
-        info.id +
-        "&bitrate=" +
-        ["999000", "320000"].slice(
-            select.ENABLE_FLAC ? 0 : 1,
-            select.ENABLE_FLAC ? 1 : 2
-        );
-    let headers = null;
-    if (!select.CAN_ACCESS_GOOGLE) headers = { host: "pyncmd.gov.cn" };
-    return request("GET", url, headers)
-        .then((response) => response.json())
-        .then((jsonBody) => {
-            if (
-                jsonBody &&
-                typeof jsonBody === "object" &&
-                "code" in jsonBody &&
-                jsonBody.code !== 200
-            )
-                return Promise.reject();
+	const url =
+		["https://pyncmd.vercel.app", "http://76.76.21.114"].slice(
+			select.CAN_ACCESS_GOOGLE ? 0 : 1,
+			select.CAN_ACCESS_GOOGLE ? 1 : 2
+		) +
+		"/api/pyncm?module=track&method=GetTrackAudio&song_ids=" +
+		info.id +
+		"&bitrate=" +
+		["999000", "320000"].slice(
+			select.ENABLE_FLAC ? 0 : 1,
+			select.ENABLE_FLAC ? 1 : 2
+		); //https://pyncmd.apis.imouto.in/api/pyncm?module=track&method=GetTrackAudio&song_ids=
+	let headers = null;
+	if (!select.CAN_ACCESS_GOOGLE) headers = { host: "pyncmd.gov.cn" };
+	return request("GET", url, headers)
+		.then((response) => response.json())
+		.then((jsonBody) => {
+			if (
+				jsonBody &&
+				typeof jsonBody === "object" &&
+				"code" in jsonBody &&
+				jsonBody.code !== 200
+			)
+				return Promise.reject();
 
-            const matched = jsonBody.data.find((song) => song.id === info.id);
-            if (matched && matched.url)
-                return {
-                    url: matched.url,
-                    weight: Number.MAX_VALUE,
-                };
+			const matched = jsonBody.data.find((song) => song.id === info.id);
+			if (matched && matched.url)
+				return {
+					url: matched.url,
+					weight: Number.MAX_VALUE,
+				};
 
-            return Promise.reject();
-        });
+			return Promise.reject();
+		});
 };
 
 const check = (info) => cache(track, info);
 
 module.exports = {
-    check,
+	check,
 };
